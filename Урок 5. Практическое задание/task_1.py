274e330231144da91aeb04b7ad3bfd23f64fb1cc
sevgenn <sevgenn@gmail.com>
2020-04-11 07:56:34 +0300

    ДЗ к пятому уроку

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,188 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+
+import timeit
+from random import randint
+from collections import Counter
+from collections import namedtuple
+from collections import deque
+
+
+"""Стандартный вариант с обычным словарем сделал не по заданию, а из любопытства,
+    чтобы в дальнейшем протестировать и сопоставить.
+    На тесте показал лучшее время: 0.1754446."""
+
+COMPANIES = {}
+N = int(input("Количество предприятий: "))
+
+
+def common_func(n):
+    """Использование обычного словаря"""
+    total_profit = 0
+    for i in range(n):
+        # name = f"common_{i}"      # использовалось для замера времени выполнения алгоритма
+        # profit = [float(randint(0, 100)) for _ in range(4)]   #
+        # использовалось для замера времени выполнения алгоритма
+        name = input(str(i + 1) + "-й предприятие: ")
+        profit = list(map(float, input(
+            'Через пробел введите прибыль данного предприятия за каждый квартал: ').split()))
+        COMPANIES[name] = profit
+        total_profit += sum(profit)
+    avrg_profit = total_profit / n
+
+    print(f'\nСредняя годовая прибыль всех предприятий: {avrg_profit}')
+    print('\nПредприятия, с прибылью выше среднего значения:')
+    for i in COMPANIES:
+        if sum(COMPANIES[i]) > avrg_profit:
+            print(i)
+    print('\nПредприятия, с прибылью ниже среднего значения:')
+    for i in COMPANIES:
+        if sum(COMPANIES[i]) < avrg_profit:
+            print(i)
+
+# common_func(N)
+# print(COMPANIES)
+
+
+
+##########################################################################
+"""В последний момент к стандартному словарю добавил DEQUE для количества.
+    Но на тесте показало: 0.16620430000000003.
+    Может быть, сэкономило на добавлении и извлечении при выводе."""
+
+# COMPANIES = {}
+# N = int(input("Количество предприятий: "))
+
+
+def deque_func(n):
+    """Использование обычного словаря и deque"""
+    total_profit = 0
+    for i in range(n):
+        # name = f"common_{i}"      # использовалось для замера времени выполнения алгоритма
+        # profit = [float(randint(0, 100)) for _ in range(4)]   #
+        # использовалось для замера времени выполнения алгоритма
+        name = input(str(i + 1) + "-й предприятие: ")
+        profit = list(map(float, input(
+            'Через пробел введите прибыль данного предприятия за каждый квартал: ').split()))
+        COMPANIES[name] = profit
+        total_profit += sum(profit)
+    avrg_profit = total_profit / n
+
+    more_avrg = deque()
+    less_avrg = deque()
+    for i in COMPANIES:
+        if sum(COMPANIES[i]) > avrg_profit:
+            more_avrg.append(i)
+        elif sum(COMPANIES[i]) < avrg_profit:
+            less_avrg.append(i)
+
+    print(f'\nСредняя годовая прибыль всех предприятий: {avrg_profit}')
+    print('\nПредприятия, с прибылью выше среднего значения:')
+    for name in more_avrg:
+        print(name)
+    print('\nПредприятия, с прибылью ниже среднего значения:')
+    for name in less_avrg:
+        print(name)
+
+# deque_func(N)
+# print(COMPANIES)
+
+
+
+##########################################################################
+"""Вариант с использованием COUNTER писал только для того, чтоб было.
+    На тесте время выполнения лишь немного уступило варианту с обычным словарем: 0.2575118."""
+
+# COMPANIES = {}
+# N = int(input("Количество предприятий: "))
+
+
+def counter_func(n):
+    """Использование Counter"""
+    for i in range(n):
+        # name = f"count_{i}"       # использовалось для замера времени выполнения алгоритма
+        # q = [float(randint(0, 100)) for _ in range(4)]    # использовалось для замера
+        name = input(str(i + 1) + "-й предприятие: ")
+        q = [float(
+            input(f'Введите по запросу прибыль данного предприятия за {j+1} квартал: '))
+             for j in range(4)]
+        COMPANIES[name] = Counter(q_1=q[0], q_2=q[1], q_3=q[2], q_4=q[3])
+
+    total = Counter()
+    for k in COMPANIES:
+        # total += COMPANIES[k]         # как вариант расчета
+        total.update(COMPANIES[k])
+    avrg_profit = sum(total.values()) / len(COMPANIES)
+
+    print(f'\nСредняя годовая прибыль всех предприятий: {avrg_profit}')
+    print('\nПредприятия, с прибылью выше среднего значения:')
+    for name in COMPANIES:
+        if sum(COMPANIES[name].values()) > avrg_profit:
+            print(name)
+    print('\nПредприятия, с прибылью ниже среднего значения:')
+    for name in COMPANIES:
+        if sum(COMPANIES[name].values()) < avrg_profit:
+            print(name)
+
+# counter_func(N)
+# print(COMPANIES)
+
+
+
+##########################################################################
+"""Вариант с использованием NAMEDTUPLE изначально напрашивался сам собой.
+    На тесте время выполнения худшее из всех вариантов: 0.30894900000000003.
+    Возможно, я где-то криво написал программу."""
+
+# COMPANIES = {}
+# N = int(input("Количество предприятий: "))
+
+
+def tuple_func(n):
+    """Использование namedtuple"""
+    Company = namedtuple('Company', 'q_1 q_2 q_3 q_4 year_profit')
+    for i in range(n):
+        # name = f"tuple_{i}"       # использовалось для замера времени выполнения алгоритма
+        # profit = [float(randint(0, 100)) for _ in range(4)]  # использовалось
+        # для замера времени выполнения алгоритма
+        name = input(str(i + 1) + "-й предприятие: ")
+        profit = [float(x) for x in input(
+            'Через пробел введите прибыль данного предприятия за каждый квартал: ').split()]
+        year_profit = sum(profit)
+        COMPANIES[name] = Company(*profit, year_profit)
+
+    total_profit = 0
+    for name in COMPANIES:
+        total_profit += COMPANIES[name].year_profit
+    avrg_profit = total_profit / len(COMPANIES)
+
+    print()
+    print(f'Средняя годовая прибыль всех предприятий: {avrg_profit}')
+    print('\nПредприятия, с прибылью выше среднего значения:')
+    for name in COMPANIES:
+        if COMPANIES[name].year_profit > avrg_profit:
+            print(name)
+    print('\nПредприятия, с прибылью ниже среднего значения:')
+    for name in COMPANIES:
+        if COMPANIES[name].year_profit < avrg_profit:
+            print(name)
+
+# tuple_func(N)
+# print(COMPANIES)
+
+
+N = 10
+COMPANIES = {}
+"""
+Тесты запускал по очереди, чтобы не было конфликтов из-за словаря COMPANIES,
+сформированного разными методами.
+"""
+######## Цикл:
+# print(timeit.timeit('common_func(n)', setup='from __main__ import common_func, n', number=1000))
+######## Counter:
+# print(timeit.timeit('counter_func(n)', setup='from __main__ import counter_func, n', number=1000))
+######## NAMEDTUPLE:
+# print(timeit.timeit('tuple_func(n)', setup='from __main__ import tuple_func, n', number=1000))
+######## DEQUE:
+# print(timeit.timeit('deque_func(n)', setup='from __main__ import deque_func, n', number=1000))
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,111 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+
+from collections import deque
+
+
+FIRST = deque(input('Enter the first number - ').upper())
+SECOND = deque(input('Enter the second number - ').upper())
+print(FIRST, SECOND)
+
+##########################################################################
+'''Самая короткая реализация'''
+
+S = hex(int(''.join(FIRST), 16) + int(''.join(SECOND), 16))
+M = hex(int(''.join(FIRST), 16) * int(''.join(SECOND), 16))
+print(int(''.join(FIRST), 16))
+print(int(''.join(SECOND), 16))
+print(S[2:])
+print(M[2:])
+
+
+
+##########################################################################
+'''Реализация с произвольным модулем через циклы с использованием DEQUE'''
+
+def sum_hex(first, second, mod=16):
+    '''Функция сложения'''
+# Генерирование базового спмска 16-ричных значений:
+    list_hex = [str(i) for i in range(10)] + [chr(j) for j in range(65, 71)]
+    # print(list_hex)
+# Выравниваем количество разрядов в складываемых числах, заполняя 0:
+    diff = abs(len(first) - len(second))
+    first.extendleft(['0'] * diff) if len(second) > len(first) else second.extendleft(['0'] * diff)
+
+    result_sum = deque()
+    rest = 0
+    for i in range(-1, -(len(first) + 1), -1):
+        result_sum.appendleft(
+            list_hex[(list_hex.index(first[i]) + list_hex.index(second[i]) + rest) % mod])
+        if (list_hex.index(first[i]) + list_hex.index(second[i]) + rest) >= mod:
+            rest = 1
+        else:
+            rest = 0
+    if rest:
+        result_sum.appendleft(1)
+    result = f"Сумма чисел списком: {list(result_sum)}\nсумма чисел строкой {''.join(result_sum)}"
+    return result
+
+# print(sum_hex(FIRST, SECOND))
+
+
+def mult_hex(first, second, mod=16):
+    '''Функция умножения'''
+    list_hex = [str(i) for i in range(10)] + [chr(j) for j in range(65, 71)]
+    # print(list_hex)
+# Создание результирующего списка с разрядностью n+m:
+    n = len(first)
+    m = len(second)
+    temp = [0] * (m + n)
+# Заполнение разрядов:
+    for i in range(n - 1, -1, -1):
+        for j in range(m - 1, -1, -1):
+            temp[i + j + 1] += list_hex.index(first[i]) * list_hex.index(second[j])
+# Обработка по модулю:
+    rest = 0
+    for k in range(-1, -(len(temp)), -1):
+        cell = temp[k] + rest
+        if (temp[k] + rest) >= mod:
+            temp[k] = (temp[k] + rest) % mod
+            rest = cell // mod
+        else:
+            rest = 0
+
+    if rest > 0:
+        temp[0] = rest
+    else:
+        temp.pop(0)
+
+    result_mult = [list_hex[x] for x in temp]
+    result = f"Произведение чисел списком: {list(result_mult)}\nпроизведение чисел строкой {''.join(result_mult)}"
+    return result
+
+
+# print(mult_hex(FIRST, SECOND))
+
+
+
+##########################################################################
+'''Реализация через класс'''
+
+class Hex_add_mult:
+    def __init__(self, num_1, num_2):
+        self.num_1 = num_1
+        self.num_2 = num_2
+
+    def __add__(self, other=0):
+        return list(hex(int(''.join(self.num_1), 16) + int(''.join(self.num_2), 16)))[2:]
+
+    def __mul__(self, othe=0):
+        return list(hex(int(''.join(self.num_1), 16) * int(''.join(self.num_2), 16)))[2:]
+        # self.mult_result = list(hex(int(''.join(self.num_1), 16) * int(''.join(other), 16)))[2:]
+        # return f"Произведение чисел списком: {self.mult_result}"
+
+
+# x = Hex_add_mult(FIRST, SECOND)
+# y = Hex_add_mult(FIRST, SECOND)
+#
+# print(x + y)
+# print(x * y)
+
