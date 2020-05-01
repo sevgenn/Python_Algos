6a262e1121f9eacdc11cd75006ef23c46fe684b2
sevgenn <sevgenn@gmail.com>
2020-05-01 04:02:12 +0300

    ДЗ к восьмому уроку

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -1,18 +1,17 @@
-"""
-Определение количества различных подстрок с использованием хеш-функции.
+'''
+Определение количества различных подстрок с использованием хэш-функции.
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
+'''
 
-Пример:
-строка: рара
 
-подстроки:
-рар
-ра
-р
-а
-ар
-ара
+import hashlib
 
-Итог: 6 подстрок
-"""
+STR_ORIGINAL = input('Enter the string - ').lower()
+SUB_STR = set()
+
+for i in range(len(STR_ORIGINAL)):
+	for j in range(i,len(STR_ORIGINAL)):
+		SUB_STR.add(hashlib.sha1(STR_ORIGINAL[i:j+1].encode('utf-8')).hexdigest())
+
+print(f'Quantity of the substrings - {len(list(SUB_STR))}')
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -1,10 +1,53 @@
-"""
+'''
 Закодируйте любую строку из трех слов по алгоритму Хаффмана.
+'''
 
-Пример:
-строка для кодирования
-s = "beep boop beer!"
-
-Результат:
-00 11 11 101 010 00 011 011 101 010 00 11 11 1000 1001
+""""
+Придумывать самостоятельное решение не стал, т.к. на данном этапе оно все равно
+будет хуже существующих вариантов.
+Посчитал, что это тот случай, когда полезнее разобраться в лучших решениях.
 """
+
+from collections import Counter, deque
+
+
+def huffman_dict(st):
+    count = Counter(st)
+    count_sorted = deque(sorted(count.items(), key=lambda item: item[1]))
+    if len(count_sorted) != 1:
+        while len(count_sorted) > 1:
+            weight = count_sorted[0][1] + count_sorted[1][1]
+            comb = {0: count_sorted.popleft()[0], 1: count_sorted.popleft()[0]}
+
+            for i, _count in enumerate(count_sorted):
+                if weight > _count[1]:
+                    continue
+                else:
+                    count_sorted.insert(i, (comb, weight))
+                    break
+            else:
+                count_sorted.append((comb, weight))
+    else:
+        weight = count_sorted[0][1]
+        comb = {0: count_sorted.popleft()[0], 1: None}
+        count_sorted.append((comb, weight))
+    return count_sorted[0][0]
+
+
+code = dict()
+
+def huffman_code(tree, path=''):
+    if not isinstance(tree, dict):
+        code[tree] = path
+    else:
+        huffman_code(tree[0], path=f'{path}0')
+        huffman_code(tree[1], path=f'{path}1')
+
+st_in = "beep boop beer!"
+
+huffman_code(huffman_dict(st_in))
+
+for i in st_in:
+    print(code[i], end=' ')
+print()
+
